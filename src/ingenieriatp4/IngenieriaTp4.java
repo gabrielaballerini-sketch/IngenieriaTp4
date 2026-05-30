55ad1ce72ceb48b10e6fc4d2b86cf0fefc804ee9
EvelynCetera <evelynyamilaa@gmail.com>
2026-05-30 03:30:19 -0300

    Cambios en la Clase Preferencia y prueba por consola de los métodos(Clase Main).

--- src/ingenieriatp4/IngenieriaTp4.java
+++ src/ingenieriatp4/IngenieriaTp4.java
@@ -5,8 +5,11 @@
  */
 package ingenieriatp4;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -18,23 +21,64 @@ public class IngenieriaTp4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
-    List<Soltero> listaSolteros = new ArrayList<>();
-    List<Lugar> listaLugares = new ArrayList<>();
-    List<Match> listaMatches = new ArrayList<>();
 
-    Lugar lugar1= new Lugar("Odisea", "Lamadrid 1790", "Sala de escape", TipoLugar.FUN);
-    Lugar lugar2=  new Lugar("Cafecito", "junin 1667", "Cafeteria de especialidad", TipoLugar.CHILL);
-    Lugar lugar3=  new Lugar("Salpicarte", "Mitre 818", "LUgar de diversion asegurada", TipoLugar.FUN);
-    
-    listaLugares.add(lugar1);
-    listaLugares.add(lugar2);
-    listaLugares.add(lugar3);
-    
+        List<Soltero> listaSolteros = new ArrayList<>();
+        List<Lugar> listaLugares = new ArrayList<>();
+        List<Match> listaMatches = new ArrayList<>();
+
+        Lugar lugar1 = new Lugar("Odisea", "Lamadrid 1790", "Sala de escape", TipoLugar.FUN);
+        Lugar lugar2 = new Lugar("Cafecito", "junin 1667", "Cafeteria de especialidad", TipoLugar.CHILL);
+        Lugar lugar3 = new Lugar("Salpicarte", "Mitre 818", "LUgar de diversion asegurada", TipoLugar.FUN);
+
+        listaLugares.add(lugar1);
+        listaLugares.add(lugar2);
+        listaLugares.add(lugar3);
+
         System.out.println(Lugar.verLugaresDeAcuerdoAlTipo("Fun", listaLugares));
+
+        Soltero soltero1 = new Soltero(12345, "Candidato", "DePueba");
+
+        Map<String, Integer> ponderacionesDePrueba = new HashMap<>();
+        ponderacionesDePrueba.put("edad", 5);
+        ponderacionesDePrueba.put("nivelCultural", 3);
+
+        Preferencias p1 = new Preferencias(EstadoCivil.SOLTERO, 1.60, 1.85, 50.0, 90.0, 25, 35, false,
+                ColorPelo.MOROCHO, ColorOjos.MARRON,
+                Nivel.ALTO,
+                Nivel.ALTO,
+                Nivel.MEDIO,
+                Nivel.ALTO,
+                Nivel.MEDIO,
+                "Lectura de novelas, running y cine",
+                LocalDate.now(),
+                ponderacionesDePrueba);
+
+        System.out.println("\n ------------ Ejecutando modificarPreferencias -----------------");
+
+        Preferencias p2 = p1.modificarPreferencias(
+                EstadoCivil.DIVORCIADO, p1.getEstaturaMin(), p1.getEstaturaMax(),
+                p1.getPesoMin(), p1.getPesoMax(), p1.getEdadMin(), p1.getEdadMax(), p1.isAceptaHijo(),
+                ColorPelo.RUBIO,
+                ColorOjos.CELESTE,
+                p1.getNivelCultural(), p1.getNivelDeCarinio(), p1.getCompanierismo(),
+                p1.getAlegre(), p1.getSalidor(), p1.getAficiones(),
+                LocalDate.now(), ponderacionesDePrueba
+        );
+
+        System.out.println("\nPreferencias (P1): " + p1.toString() + "\n");
+        System.out.println("Preferencias (P2): " + p2.toString() + "\n");
+
+        if (p1 != p2 && p1.getColorOjosBusc() == ColorOjos.MARRON) {
+            System.out.println("Se modifico la preferencia correctamente");
+        } else {
+            System.out.println("Error al modificar preferencias.");
+        }
+
+        System.out.println("\n ------------ Ejecutando calcularAfinidad -----------------");
+        
+        int resultAfinidad = p1.calcularAfinidadCon(soltero1);
+        System.out.println("\n Afinidad calculada con s1: " + resultAfinidad);
     
-        
-        
     }
     
 }
--- src/ingenieriatp4/Preferencias.java
+++ src/ingenieriatp4/Preferencias.java
@@ -1,42 +1,240 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ingenieriatp4;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
- * @author Capotes
+ * @author Cetera Evelyn
  */
 public class Preferencias {
     
-    
-    
-    
-    
-private double estatura;
-private double peso;
-private Enum colorPelo;
-private Enum colorOjos;
-private Enum nivelCultural;
-private Enum nivelDeCariño;
-private Enum compañerismo;
-private Enum alegre;
-private Enum salidor;
-List<String> aficciones;
-    
-    
+    private EstadoCivil estadoCivilBusc;
+    private double estaturaMin, estaturaMax;
+    private double pesoMin, pesoMax;
+    private int edadMin, edadMax;
+    private boolean aceptaHijo;
+    private ColorPelo colorPeloBusc;
+    private ColorOjos colorOjosBusc;
+    private Nivel nivelCultural;
+    private Nivel nivelDeCarinio;
+    private Nivel companierismo;
+    private Nivel alegre;
+    private Nivel salidor;
+    private String aficiones;
+    private LocalDate fechaModificacion;
+    private Map <String, Integer> ponderaciones;
+
+    public Preferencias(EstadoCivil estadoCivilBusc, double estaturaMin, double estaturaMax, double pesoMin, double pesoMax, int edadMin, int edadMax, boolean aceptaHijo, 
+            ColorPelo colorPeloBusc, ColorOjos colorOjosBusc, Nivel nivelCultural, Nivel nivelDeCarinio, Nivel companierismo, Nivel alegre, Nivel salidor, String aficiones, 
+            LocalDate fechaModificacion, Map<String, Integer> ponderaciones) {
+        this.estadoCivilBusc = estadoCivilBusc;
+        this.estaturaMin = estaturaMin;
+        this.estaturaMax = estaturaMax;
+        this.pesoMin = pesoMin;
+        this.pesoMax = pesoMax;
+        this.edadMin = edadMin;
+        this.edadMax = edadMax;
+        this.aceptaHijo = aceptaHijo;
+        this.colorPeloBusc = colorPeloBusc;
+        this.colorOjosBusc = colorOjosBusc;
+        this.nivelCultural = nivelCultural;
+        this.nivelDeCarinio = nivelDeCarinio;
+        this.companierismo = companierismo;
+        this.alegre = alegre;
+        this.salidor = salidor;
+        this.aficiones = aficiones;
+        this.fechaModificacion = fechaModificacion;
+        this.ponderaciones = ponderaciones;
+    }
+
+    public EstadoCivil getEstadoCivilBusc() {
+        return estadoCivilBusc;
+    }
+
+    public void setEstadoCivilBusc(EstadoCivil estadoCivilBusc) {
+        this.estadoCivilBusc = estadoCivilBusc;
+    }
+
+    public double getEstaturaMin() {
+        return estaturaMin;
+    }
+
+    public void setEstaturaMin(double estaturaMin) {
+        this.estaturaMin = estaturaMin;
+    }
+
+    public double getEstaturaMax() {
+        return estaturaMax;
+    }
+
+    public void setEstaturaMax(double estaturaMax) {
+        this.estaturaMax = estaturaMax;
+    }
+
+    public double getPesoMin() {
+        return pesoMin;
+    }
+
+    public void setPesoMin(double pesoMin) {
+        this.pesoMin = pesoMin;
+    }
+
+    public double getPesoMax() {
+        return pesoMax;
+    }
+
+    public void setPesoMax(double pesoMax) {
+        this.pesoMax = pesoMax;
+    }
+
+    public int getEdadMin() {
+        return edadMin;
+    }
+
+    public void setEdadMin(int edadMin) {
+        this.edadMin = edadMin;
+    }
+
+    public int getEdadMax() {
+        return edadMax;
+    }
+
+    public void setEdadMax(int edadMax) {
+        this.edadMax = edadMax;
+    }
+
+    public boolean isAceptaHijo() {
+        return aceptaHijo;
+    }
+
+    public void setAceptaHijo(boolean aceptaHijo) {
+        this.aceptaHijo = aceptaHijo;
+    }
+
+    public ColorPelo getColorPeloBusc() {
+        return colorPeloBusc;
+    }
+
+    public void setColorPeloBusc(ColorPelo colorPeloBusc) {
+        this.colorPeloBusc = colorPeloBusc;
+    }
+
+    public ColorOjos getColorOjosBusc() {
+        return colorOjosBusc;
+    }
+
+    public void setColorOjosBusc(ColorOjos colorOjosBusc) {
+        this.colorOjosBusc = colorOjosBusc;
+    }
+
+    public Nivel getNivelCultural() {
+        return nivelCultural;
+    }
+
+    public void setNivelCultural(Nivel nivelCultural) {
+        this.nivelCultural = nivelCultural;
+    }
+
+    public Nivel getNivelDeCarinio() {
+        return nivelDeCarinio;
+    }
+
+    public void setNivelDeCarinio(Nivel nivelDeCarinio) {
+        this.nivelDeCarinio = nivelDeCarinio;
+    }
+
+    public Nivel getCompanierismo() {
+        return companierismo;
+    }
+
+    public void setCompanierismo(Nivel companierismo) {
+        this.companierismo = companierismo;
+    }
+
+    public Nivel getAlegre() {
+        return alegre;
+    }
+
+    public void setAlegre(Nivel alegre) {
+        this.alegre = alegre;
+    }
+
+    public Nivel getSalidor() {
+        return salidor;
+    }
+
+    public void setSalidor(Nivel salidor) {
+        this.salidor = salidor;
+    }
+
+    public String getAficiones() {
+        return aficiones;
+    }
+
+    public void setAficiones(String aficiones) {
+        this.aficiones = aficiones;
+    }
+
+    public LocalDate getFechaModificacion() {
+        return fechaModificacion;
+    }
+
+    public void setFechaModificacion(LocalDate fechaModificacion) {
+        this.fechaModificacion = fechaModificacion;
+    }
+
+    public int getPonderaciones(String claveAtributo) {
+        if (this.ponderaciones.containsKey(claveAtributo)) {
+            return this.ponderaciones.get(claveAtributo);
+        } else {
+            return 1; 
+        }
+    }
+
+    public void setPonderaciones(Map<String, Integer> ponderaciones) {
+        this.ponderaciones = ponderaciones;
+    }
+    public String trueOrFalse (boolean aceptaHijo){ //metodo para cambiar los valores de los atributos booleanos... por si o por no. Meramente gráfico para la consola!!!
+        String valor;
+        if (aceptaHijo){
+            valor = "SI";
+        }else {
+            valor = "NO";
+        }
+        return valor;
+    }
+    @Override
+    public String toString() {
+        return "Preferencias{" + "Estado Civil Buscado: " + estadoCivilBusc + ",\n"
+                + "Estatura Min: [" + estaturaMin + "] - Estatura Max: [" + estaturaMax + "],\n Peso Min: [" + pesoMin + "] - Peso Max: " + pesoMax + ",\n"
+                + "Edad Min: [" + edadMin + "] -  Edad Max: [" + edadMax + "]\n Acepta Hijo: [" + trueOrFalse(aceptaHijo) + "],\n Color de Pelo: " + colorPeloBusc + ",\n Color de Ojos: " + colorOjosBusc + "\n" 
+                + "Nivel Cultural: " + nivelCultural + ",\n Nivel De Carinio: " + nivelDeCarinio + ",\n Companierismo: " + companierismo + ",\n Alegre: " + alegre + ",\n Salidor: " + salidor + "\n" 
+                + "Aficiones: " + aficiones + ",\n Fecha de Modificacion: [" + fechaModificacion + "],\n Ponderaciones: " + ponderaciones + '}';
+    }
 
 
 
-public void add(){};
 
-public void modificarPreferencias(){}
+    public Preferencias modificarPreferencias(
+        EstadoCivil estCiv, double nuevaEstaturaMin, double nuevaEstaturaMax, double nuevaPesoMin, double nuevaPesoMax, int nuevaEdadMin, int nuevaEdadMax, boolean nuevaAceptaHijo, 
+            ColorPelo nuevoColorPelo, ColorOjos nuevoColorOjos, Nivel nuevoNivCultural, Nivel nuevoNivelDeCarinio, Nivel nuevoCompanierismo, Nivel nuevoAlegre, Nivel nuevoSalidor, String nuevasAficiones, 
+            LocalDate nuevaFecha, Map<String, Integer> nuevasPonderaciones) {
+        return new Preferencias(
+            estCiv, nuevaEstaturaMin, nuevaEstaturaMax, nuevaPesoMin, nuevaPesoMax, nuevaEdadMin, nuevaEdadMax,  nuevaAceptaHijo, 
+             nuevoColorPelo, nuevoColorOjos, nuevoNivCultural, nuevoNivelDeCarinio, nuevoCompanierismo, nuevoAlegre,  nuevoSalidor, nuevasAficiones, 
+             nuevaFecha, nuevasPonderaciones
+        );
+    }
 
-public void calcularAfinidad(){}
-    
+
+
+    public int calcularAfinidadCon(Soltero candidato) {
+        int afinidadSimulada = 8;
+        return afinidadSimulada;
+    }
     
 }
